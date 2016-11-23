25f6c168f80deb207bc8cf997b3fa317ca2d10a1
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-11-23 20:48:44 +0100

    Fix path

--- concourse/build.sh
+++ concourse/build.sh
@@ -4,4 +4,4 @@ set -o errexit
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}/../"
 ant binary-distribution
-cp dist/lazabs.jar "${DIR}/../eldarica-jar/"
+cp dist/lazabs.jar "${DIR}/../../eldarica-jar/"
